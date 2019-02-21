0b519bb9f9ca08fc6a90a1cd7b5acd8d3757e64a
Tom Lenz <Thomas.Lenz@LenzConsulting.net>
2019-02-21 02:54:46 -0600

    Added additional API files

---
+++ AdvisingSystemAPI/src/main/java/edu/lewisu/cs/api/course/CourseManagementApplication.java
@@ -0,0 +1,21 @@
+package edu.lewisu.cs.api.course;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
+import java.util.Set;
+import java.util.HashSet;
+
+import edu.lewisu.cs.api.course.CourseManagementService;
+import edu.lewisu.cs.api.course.CourseCreateService;
+import edu.lewisu.cs.api.user.UserManagementService;
+import edu.lewisu.cs.api.sched.ScheduleBuilderService;
+
+@ApplicationPath("/CourseManagement/*")
+public class CourseManagementApplication extends javax.ws.rs.core.Application {
+   public Set<Class<?>> getClasses() {
+      Set<Class<?>> s = new HashSet<Class<?>>();
+      s.add(CourseCreateService.class);
+
+      return s;
+    }
+}
